0c83356e3e5053e588bb4d488345391091123457
daniela <danielaborja1529@gmail.com>
2021-11-11 23:08:57 -0500

    laboratorio 5

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -6,13 +6,33 @@ module.exports = async function (context, req) {
         context.res = {
             status= 200,
             body: "Hello, " + name
-
+        };
     }
     else{
         context.res = {
             status= 400,
             body: "Hello, " + name
     }
+    module.exports = async function (context, req) {
+        context.log('JavaScript HTTP trigger function 1.');
+    
+        const name = (req.query.name || (req.body && req.body.name));
+        context.log('JavaScript HTTP trigger function 2.');
+    
+        if(name)
+        {
+            context.res = {
+                status: 200, /* Defaults to 200 */
+                body: "Welcome, " + name
+            };
+        }else{
+            context.res = {
+                status: 400, 
+                body: "Pass a name in the query string or in the request body"
+            };
+        }
+    
+    } ;
     const responseMessage = name
         ? "Hello, " + name
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
